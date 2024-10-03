7421bb0479fbdb140be0b0a3ff19b73b9a3ccfa0
John Ferdie Abueg <john.abueg@gmail.com>
2024-10-03 20:42:28 +0200

    ViewPost and ViewProfile components

---
+++ src/Components/ViewPost.jsx
@@ -0,0 +1,7 @@
+export default function ViewPost() {
+	return (
+		<>
+			<h1>View post</h1>
+		</>
+	);
+}
---
+++ src/Components/ViewProfile.jsx
@@ -0,0 +1,7 @@
+export default function ViewProfile() {
+	return (
+		<>
+			<h1>View profile</h1>
+		</>
+	);
+}
